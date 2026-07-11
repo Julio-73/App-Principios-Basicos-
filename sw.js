7425f6ddabca3d9b03b5f78acba42b7afbe42155
Julio Cesar Quispe Garrido <juliocesarquispegarrido@gmail.com>
2026-07-11 15:57:52 +0200

    feat: optimiza visualmente la cabecera, agrega gradiente de desvanecimiento al menu superior, aumenta margen inferior y rediseña boton de guardar notas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ═══════════════════════════════════════════════════════ SERVICE WORKER - APP OFFLINE ═══════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'principios-basicos-v13';
+const CACHE_NAME = 'principios-basicos-v14';
 const urlsToCache = [
   './',
   './index.html',
