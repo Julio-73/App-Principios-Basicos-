e25d90196531315482add03fa29aecd92de91f2b
Julio Cesar Quispe Garrido <juliocesarquispegarrido@gmail.com>
2026-07-18 16:10:09 +0200

    feat: implementa menu inferior (Bottom Sheet) nativo en dispositivos moviles para verseModal con manija visual

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ═══════════════════════════════════════════════════════ SERVICE WORKER - APP OFFLINE ═══════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'principios-basicos-v24';
+const CACHE_NAME = 'principios-basicos-v25';
 const urlsToCache = [
   './',
   './index.html',
